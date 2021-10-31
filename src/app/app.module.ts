f6079374d64b492043af5fe6663445fb7ee09cf1
Omri <mareberao@gmail.com>
2021-10-31 20:13:51 +0200

    initial setup of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './pages/layouts/topbar/topbar.component';
+import { SidebarComponent } from './pages/layouts/sidebar/sidebar.component';
+import { ProductListComponent } from './pages/products/product-list/product-list.component';
+import { ProductSingleComponent } from './pages/products/product-single/product-single.component';
+import { AutobidComponent } from './pages/bids/autobid/autobid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    SidebarComponent,
+    ProductListComponent,
+    ProductSingleComponent,
+    AutobidComponent
   ],
   imports: [
     BrowserModule,
