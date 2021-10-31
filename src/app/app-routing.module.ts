7d2e1860fee105349bcd67c9fed49b6bb6f7baaa
Omri <mareberao@gmail.com>
2021-10-31 22:36:25 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ProductListComponent } from './pages/products/product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
