b2cc6eeb43b994f8390d63734d9801dcbec1fd3c
Omri <mareberao@gmail.com>
2021-11-01 13:30:17 +0200

    +

--- src/app/pages/products/product-list/product-list.component.ts
+++ src/app/pages/products/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { DataService } from './../../../services/data.service';
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-product-list',
@@ -10,19 +11,22 @@ export class ProductListComponent implements OnInit {
 
   allProducts : any= [];
 
-  constructor(private data : DataService) { }
+  constructor(private data : DataService,private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.getAllProducts();
   }
 
   getAllProducts(): void {
+    this.spinner.show();
     this.data.getAllProducts().subscribe(
       (data: any) => {
         this.allProducts = data.content;
-        console.log(this.allProducts);
+        this.spinner.hide();
       }
-    );
-  }
+  ,(error) => {
+    alert('Data Fetch Error')
+    this.spinner.hide();
+  }) }
 
-}
+  }
