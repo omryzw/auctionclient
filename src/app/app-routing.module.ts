62504bd36e48b9212ad0a4d1336d7583f6b7df87
Omri <mareberao@gmail.com>
2021-11-01 10:17:15 +0200

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { ProductSingleComponent } from './pages/products/product-single/product-single.component';
+import { LoginComponent } from './pages/layouts/login/login.component';
 import { ProductListComponent } from './pages/products/product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -5,15 +7,20 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    component: ProductListComponent
+    component: ProductListComponent,
   },
-   {
-path
-  }
+  {
+    path: 'product',
+    component: ProductSingleComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
